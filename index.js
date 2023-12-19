4c24a5d2e0a9af992e847f4c63c79ba24fbea633
Ali Shokair <alishkeir_@hotmail.com>
2023-12-19 04:03:19 +0200

    Added Query variables support

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ const resolvers = {
     games() {
       return db.games;
     },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
     authors() {
       return db.authors;
     },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
     reviews() {
       return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
     },
   },
 };
@@ -26,7 +35,7 @@ const resolvers = {
 const server = new ApolloServer({
   // typeDefs -- definitions of types of data
   typeDefs,
-  
+
   resolvers,
 });
 
