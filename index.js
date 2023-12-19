6696674ca8f78407d91e3795ba5f4f17e902c2c9
Ali Shokair <alishkeir_@hotmail.com>
2023-12-19 03:50:13 +0200

    Added mock data and resolvers

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// data
+import db from './_db.js';
+
 // types
 import { typeDefs } from './schema';
+
+// resolvers
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 // server setup
 const server = new ApolloServer({
   // typeDefs -- definitions of types of data
   typeDefs,
+  
+  resolvers,
 });
 
 // start server
